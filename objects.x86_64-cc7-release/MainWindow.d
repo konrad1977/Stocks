7610f3d4cb93cffc50643011c72728a77555b6ca
Mikael Konradsson <mikael.konradsson@outlook.com>
2018-07-08 11:34:32 +0000

    initial

--- objects.x86_64-cc7-release/MainWindow.d
+++
@@ -1,6 +0,0 @@
-# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS FOLLOW ---
-objects.x86_64-cc7-release/MainWindow.o : Source/Views/StockView.h \
-    Source/StockRequester.h Source/MainWindow.h \
-    Source/Requesters/NetRequester.h
-
-# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS PRECEDE ---
